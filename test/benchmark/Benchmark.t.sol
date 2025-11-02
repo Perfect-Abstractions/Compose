e55bd28cf56fe009231faf5811b19135d39864ad
lumoswiz <lumoswiz@protonmail.com>
2025-11-02 19:42:45 +0100

    test: add test utils for setting FacetAndPosition storage

--- test/benchmark/Benchmark.t.sol
+++ test/benchmark/Benchmark.t.sol
@@ -10,6 +10,8 @@ contract LoupeGasBenchmarkTest is Test {
     MinimalDiamond internal diamond;
     DiamondLoupeFacet internal loupe;
 
+    bytes32 internal constant DIAMOND_STORAGE_POSITION = keccak256("compose.diamond");
+
     function setUp() public {
         loupe = new DiamondLoupeFacet();
         diamond = new MinimalDiamond();
@@ -31,5 +33,17 @@ contract LoupeGasBenchmarkTest is Test {
         MinimalDiamond.DiamondArgs memory args = MinimalDiamond.DiamondArgs({init: address(0), initCalldata: ""});
 
         diamond.initialize(dc, args);
+    }
+
+    function _facetAndPositionsSlot(bytes4 selector) internal pure returns (bytes32) {
+        return keccak256(abi.encode(selector, DIAMOND_STORAGE_POSITION));
+    }
+
+    function _packFacetAndPosition(address facet, uint16 position) internal pure returns (bytes32) {
+        return bytes32((uint256(uint160(facet))) | (uint256(position) << 160));
+    }
+
+    function _storeFacetAndPosition(address account, bytes4 selector, address facet, uint16 position) internal {
+        vm.store(account, _facetAndPositionsSlot(selector), _packFacetAndPosition(facet, position));
     }
 }
