5ecaad19755712bee1e18f31868c0d6fa6660749
lumoswiz <lumoswiz@protonmail.com>
2025-11-02 20:41:54 +0100

    test: build complex diamond helpers

--- test/benchmark/Benchmark.t.sol
+++ test/benchmark/Benchmark.t.sol
@@ -11,6 +11,8 @@ contract LoupeGasBenchmarkTest is Test {
     DiamondLoupeFacet internal loupe;
 
     bytes32 internal constant DIAMOND_STORAGE_POSITION = keccak256("compose.diamond");
+    uint256 internal constant NUM_FACETS = 200;
+    uint256 internal constant SELECTORS_PER_FACET = 10;
 
     function setUp() public {
         loupe = new DiamondLoupeFacet();
@@ -33,6 +35,8 @@ contract LoupeGasBenchmarkTest is Test {
         MinimalDiamond.DiamondArgs memory args = MinimalDiamond.DiamondArgs({init: address(0), initCalldata: ""});
 
         diamond.initialize(dc, args);
+
+        _buildDiamond(address(diamond), NUM_FACETS, SELECTORS_PER_FACET);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -73,5 +77,38 @@ contract LoupeGasBenchmarkTest is Test {
         uint256 newPackedWord = (uint256(oldPackedWord) & clearLaneMask) | laneInsertBits;
 
         vm.store(account, packedWordSlot, bytes32(newPackedWord));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              DATA HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    function _buildDiamond(address account, uint256 nFacets, uint256 perFacet) internal {
+        uint256 total = nFacets * perFacet;
+        vm.store(account, _selectorsLengthSlot(), bytes32(total));
+
+        uint256 globalIndex = 4;
+        for (uint256 f = 0; f < nFacets; f++) {
+            address facet = _facetAddr(f);
+            for (uint16 j = 0; j < perFacet; j++) {
+                bytes4 selector = _selectorFor(f, j);
+
+                _storeSelectorAtIndex(account, selector, globalIndex);
+
+                _storeFacetAndPosition(account, selector, facet, j);
+
+                unchecked {
+                    ++globalIndex;
+                }
+            }
+        }
+    }
+
+    function _facetAddr(uint256 f) internal returns (address) {
+        return makeAddr(string.concat("facet ", vm.toString(f)));
+    }
+
+    function _selectorFor(uint256 f, uint16 j) internal pure returns (bytes4) {
+        return bytes4(keccak256(abi.encodePacked("fn_f_", vm.toString(f), "_idx_", vm.toString(j), "()")));
     }
 }
