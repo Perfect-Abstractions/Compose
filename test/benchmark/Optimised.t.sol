28ea33b6caf2930e54df588b595451f670f7440c
lumoswiz <lumoswiz@protonmail.com>
2025-11-02 21:51:09 +0100

    refactor: abstract base gas loupe benchmarking contract with usage

--- test/benchmark/Optimised.t.sol
+++ test/benchmark/Optimised.t.sol
@@ -4,11 +4,13 @@ pragma solidity >=0.8.30;
 import {BaseBenchmark} from "./Base.t.sol";
 import {DiamondLoupeFacet} from "../../src/diamond/DiamondLoupeFacet.sol";
 
-contract LoupeGasBenchmarkTest is BaseBenchmark {
+/// @dev Please override `_deployLoupe` with the deployment of your optimised contract.
+contract OptimisedLoupeBenchmarkTest is BaseBenchmark {
     /*//////////////////////////////////////////////////////////////
                                OVERRIDES
     //////////////////////////////////////////////////////////////*/
 
+    /// @dev Override with a deployment of your optimised diamond loupe contract.
     function _deployLoupe() internal override returns (address) {
         return address(new DiamondLoupeFacet());
     }
@@ -17,17 +19,20 @@ contract LoupeGasBenchmarkTest is BaseBenchmark {
                              GAS BENCHMARKS
     //////////////////////////////////////////////////////////////*/
 
-    // Estimated gas: 370_049_638
+    struct Facet {
+        address facet;
+        bytes4[] functionSelectors;
+    }
+
     function testGas_Loupe_Facets() external {
         uint256 startGas = gasleft();
         (bool success, bytes memory data) = address(diamond).call(abi.encodeWithSelector(SELECTOR_FACETS));
         emit log_uint(startGas - gasleft());
 
-        DiamondLoupeFacet.Facet[] memory allFacets = abi.decode(data, (DiamondLoupeFacet.Facet[]));
+        Facet[] memory allFacets = abi.decode(data, (Facet[]));
         assertEq(allFacets.length, NUM_FACETS + 1); // plus Loupe
     }
 
-    // Estimated gas: 5_889_500
     function testGas_Loupe_FacetFunctionSelectors() external {
         uint256 startGas = gasleft();
         (bool success, bytes memory data) =
@@ -38,7 +43,6 @@ contract LoupeGasBenchmarkTest is BaseBenchmark {
         assertEq(facetSelectors.length, NUM_LOUPE_SELECTORS);
     }
 
-    // Estimated gas: 31_606_629
     function testGas_Loupe_FacetAddresses() external {
         uint256 startGas = gasleft();
         (bool success, bytes memory data) = address(diamond).call(abi.encodeWithSelector(SELECTOR_FACET_ADDRESSES));
@@ -48,7 +52,6 @@ contract LoupeGasBenchmarkTest is BaseBenchmark {
         assertEq(allFacets.length, NUM_FACETS + 1); // plus Loupe
     }
 
-    // Estimated gas: 12_672
     function testGas_Loupe_FacetAddress() external {
         uint256 startGas = gasleft();
         (bool success, bytes memory data) =
