32da46f1369b487e7e36f69e7c8239dbaa2ad1fd
lumoswiz <lumoswiz@protonmail.com>
2025-11-03 11:02:05 +0100

    feat: implement LibERC6909 functions

--- src/token/ERC6909/LibERC6909.sol
+++ src/token/ERC6909/LibERC6909.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.30;
 /// @notice Provides internal functions and storage layout for ERC-6909 minimal multi-token logic.
 /// @dev Uses ERC-8042 for storage location standardization and ERC-6093 for error conventions.
 ///      This library is intended to be used by custom facets to integrate with ERC-6909 functionality.
+/// @dev Adapted from: https://github.com/Vectorized/solady/blob/main/src/tokens/ERC6909.sol
 library LibERC6909 {
     /// @notice Thrown when owner balance for id is insufficient.
     error ERC6909InsufficientBalance();
@@ -47,32 +48,75 @@ library LibERC6909 {
     /// @param _to The address of the receiver.
     /// @param _id The id of the token.
     /// @param _amount The amount of the token.
-    function mint(address _to, uint256 _id, uint256 _amount) internal {}
+    function mint(address _to, uint256 _id, uint256 _amount) internal {
+        ERC6909Storage storage s = getStorage();
+
+        s.balanceOf[_to][_id] += _amount;
+
+        emit Transfer(msg.sender, address(0), _to, _id, _amount);
+    }
 
     /// @notice Burns `_amount` of token id `_id` from `_from`.
     /// @param _from The address of the sender.
     /// @param _id The id of the token.
     /// @param _amount The amount of the token.
-    function burn(address _from, uint256 _id, uint256 _amount) internal {}
+    function burn(address _from, uint256 _id, uint256 _amount) internal {
+        ERC6909Storage storage s = getStorage();
+
+        s.balanceOf[_from][_id] -= _amount;
+
+        emit Transfer(msg.sender, _from, address(0), _id, _amount);
+    }
 
     /// @notice Transfers `_amount` of token id `_id` from `_from` to `_to`.
+    /// @dev Allowance is not deducted if it is `type(uint256).max`
+    /// @dev Allowance is not deducted if `_by` is an operator for `_from`.
     /// @param _by The address initiating the transfer.
     /// @param _from The address of the sender.
     /// @param _to The address of the receiver.
     /// @param _id The id of the token.
     /// @param _amount The amount of the token.
-    function transfer(address _by, address _from, address _to, uint256 _id, uint256 _amount) internal {}
+    function transfer(address _by, address _from, address _to, uint256 _id, uint256 _amount) internal {
+        ERC6909Storage storage s = getStorage();
+
+        if (_by != address(0) && !s.isOperator[_from][_by]) {
+            uint256 allowed = s.allowance[_from][_by][_id];
+            if (allowed != type(uint256).max) {
+                if (_amount > allowed) revert ERC6909InsufficientPermission();
+                s.allowance[_from][_by][_id] = allowed - _amount;
+            }
+        }
+
+        if (_amount > s.balanceOf[_from][_id]) revert ERC6909InsufficientBalance();
+
+        s.balanceOf[_from][_id] -= _amount;
+        s.balanceOf[_to][_id] += _amount;
+
+        emit Transfer(_by, _from, _to, _id, _amount);
+    }
 
     /// @notice Approves an amount of an id to a spender.
     /// @param _owner The token owner.
     /// @param _spender The address of the spender.
     /// @param _id The id of the token.
     /// @param _amount The amount of the token.
-    function approve(address _owner, address _spender, uint256 _id, uint256 _amount) internal {}
+    function approve(address _owner, address _spender, uint256 _id, uint256 _amount) internal {
+        ERC6909Storage storage s = getStorage();
+
+        s.allowance[_owner][_spender][_id] = _amount;
+
+        emit Approval(_owner, _spender, _id, _amount);
+    }
 
     /// @notice Sets or removes a spender as an operator for the caller.
     /// @param _owner The address of the owner.
     /// @param _spender The address of the spender.
     /// @param _approved The approval status.
-    function setOperator(address _owner, address _spender, bool _approved) internal {}
+    function setOperator(address _owner, address _spender, bool _approved) internal {
+        ERC6909Storage storage s = getStorage();
+
+        s.isOperator[_owner][_spender] = _approved;
+
+        emit OperatorSet(_owner, _spender, _approved);
+    }
 }
