60c77b2a59326d1f1b6afa21da699148ad8391aa
Nick Mudge <nick@perfectabstractions.com>
2025-11-03 19:45:06 -0500

    Fixed edit links in website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Perfect-Abstractions/Compose/tree/main/website/docs/',
+            'https://github.com/Perfect-Abstractions/Compose/tree/main/website/',
         },
         blog: {
           showReadingTime: true,
@@ -116,7 +116,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Perfect-Abstractions/Compose/tree/main/website/blog/',
+            'https://github.com/Perfect-Abstractions/Compose/tree/main/website/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
