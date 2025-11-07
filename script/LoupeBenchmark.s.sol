0fa2ba253ed93b64540b75a908beca91a7589e49
LucaSforza <lucasforza1234@icloud.com>
2025-11-07 14:42:26 +0100

    better code stile

--- script/LoupeBenchmark.s.sol
+++ script/LoupeBenchmark.s.sol
@@ -16,7 +16,13 @@ contract LoupeBenchmarkScript is Utils {
         uint16 selectors;
     }
 
+    struct Impl {
+        string name;
+        address loupe;
+    }
+
     Config[] public configs;
+    Impl[] public impls;
 
     string constant CSV_FILE = "./benchmark.csv";
 
@@ -25,10 +31,37 @@ contract LoupeBenchmarkScript is Utils {
     ) internal {
         vm.startBroadcast();
 
-        address loupeImpl = address(new DiamondLoupeFacet());
-        string memory name = "Original";
+        // Init CSV file
         vm.writeLine(CSV_FILE,"Implementation,Function,Facets,Selectors,GasUsed");
 
+        _init_implentations();
+        _init_configs(huge_config);
+        
+        for(uint256 j=0; j < impls.length; j++) {
+            for (uint256 i = 0; i < configs.length; i++) {
+                Config memory cfg = configs[i];
+                Impl memory impl = impls[j];
+                _run_test(impl.loupe, impl.name, cfg.facets, cfg.selectors);
+            }
+        }
+
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        _run(false);
+    }
+
+    function runWithHugeConfig() external {
+        _run(true);
+    }
+
+    function _init_implentations() internal {
+        // TODO: add more implementations
+        impls.push(Impl("Original", address(new DiamondLoupeFacet())));
+    }
+
+    function _init_configs(bool huge_config) internal {
         // Test cases 
         configs.push(Config(0, 0));
         configs.push(Config(2, 1));
@@ -50,52 +83,42 @@ contract LoupeBenchmarkScript is Utils {
             configs.push(Config(10000, 834));
             configs.push(Config(40000, 5000));
         }
-        for (uint256 i = 0; i < configs.length; i++) {
-            Config memory cfg = configs[i];
-            MinimalDiamond diamond = _createTest(loupeImpl, cfg.facets, cfg.selectors);
+    }
 
-            uint256 gasStart = gasleft();
-            (bool ok1,) = address(diamond).call(abi.encodeWithSelector(SELECTOR_FACETS));
-            uint256 gasUsed1 = gasStart - gasleft();
-            string memory gasUsed1s;
-            if(ok1) {
-                gasUsed1s = vm.toString(gasUsed1);
-            } else {
-                gasUsed1s = "out of gas";
-            }
-            vm.writeLine(CSV_FILE,string.concat(
-                        name, ",facets(),",
-                        vm.toString(cfg.facets), ",",
-                        vm.toString(cfg.selectors), ",",
-                        gasUsed1s
-                    ));
+    function _run_test(address loupe,string memory name, uint256 nFacet, uint256 perFacet) internal {
+        MinimalDiamond diamond = _createTest(loupe, nFacet, perFacet);
 
-            gasStart = gasleft();
-            (bool ok2,) = address(diamond).call(abi.encodeWithSelector(SELECTOR_FACET_ADDRESSES));
-            uint256 gasUsed2 = gasStart - gasleft();
-            string memory gasUsed2s;
-            if(ok2) {
-                gasUsed2s = vm.toString(gasUsed2);
-            } else {
-                gasUsed2s = "out of gas";
-            }
-            vm.writeLine(CSV_FILE,string.concat(
-                        name, ",facetAddresses(),",
-                        vm.toString(cfg.facets), ",",
-                        vm.toString(cfg.selectors), ",",
-                        gasUsed2s
-                    ));
+        uint256 gasStart = gasleft();
+        (bool ok1,) = address(diamond).call(abi.encodeWithSelector(SELECTOR_FACETS));
+        uint256 gasUsed1 = gasStart - gasleft();
+        string memory gasUsed1s;
+        if(ok1) {
+            gasUsed1s = vm.toString(gasUsed1);
+        } else {
+            gasUsed1s = "out of gas";
         }
+        vm.writeLine(CSV_FILE,string.concat(
+                    name, ",facets(),",
+                    vm.toString(nFacet), ",",
+                    vm.toString(perFacet), ",",
+                    gasUsed1s
+                ));
 
-        vm.stopBroadcast();
-    }
-
-    function run() external {
-        _run(false);
-    }
-
-    function runWithHugeConfig() external {
-        _run(true);
+        gasStart = gasleft();
+        (bool ok2,) = address(diamond).call(abi.encodeWithSelector(SELECTOR_FACET_ADDRESSES));
+        uint256 gasUsed2 = gasStart - gasleft();
+        string memory gasUsed2s;
+        if(ok2) {
+            gasUsed2s = vm.toString(gasUsed2);
+        } else {
+            gasUsed2s = "out of gas";
+        }
+        vm.writeLine(CSV_FILE,string.concat(
+                    name, ",facetAddresses(),",
+                    vm.toString(nFacet), ",",
+                    vm.toString(perFacet), ",",
+                    gasUsed2s
+                ));
     }
 
     function _createTest(address loupe, uint256 nFacet, uint256 perFacet)
