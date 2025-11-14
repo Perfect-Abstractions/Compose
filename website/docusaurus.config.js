b64d4388ea95789d9d6c025be3a8d62a9dfb3dba
Nick Mudge <nick@perfectabstractions.com>
2025-11-13 20:55:04 -0500

    docs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
 
   // Set the production url of your site here
   //url: 'https://compose.diamonds/',
-  url: 'https://preview.compose.diamonds/',
+  url: 'https://compose.diamonds/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
