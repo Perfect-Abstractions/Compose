ce9b9a2fdd97812422293c6ea7cab291e9982552
Abhivansh <31abhivanshj@gmail.com>
2025-11-21 03:09:11 +0530

    WIP: refactor ERC20Facet

---
+++ test/token/ERC20/ERC20/harnesses/LibERC20PermitFacetHarness.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.30;
+
+import {ERC20PermitFacet} from "../../../../../src/token/ERC20/ERC20/ERC20PermitFacet.sol";
+
+/// @title ERC20BurnFacetHarness
+/// @notice Test harness for ERC20BurnFacet that adds initialization and minting for testing
+contract ERC20BurnFacetHarness is ERC20BurnFacet {
+    /// @notice Initialize the ERC20 token storage
+    /// @dev Only used for testing - production diamonds should initialize in constructor
+    function initialize(string _name, uint256 _totalSupply) external {
+        ERC20PermitStorage storage s = getStorage();
+        s.name = _name;
+        s.totalSupply = _totalSupply;
+    }
+
+    /// @notice Mint tokens to an address
+    /// @dev Only used for testing - exposes internal mint functionality
+    function mint(address _to, uint256 _value) external {
+        ERC20PermitStorage storage s = getStorage();
+        if (_to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        unchecked {
+            s.totalSupply += _value;
+            s.balanceOf[_to] += _value;
+        }
+        emit Transfer(address(0), _to, _value);
+    }
+}
