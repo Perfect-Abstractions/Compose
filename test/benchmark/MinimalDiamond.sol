48d7794e8853243ca358f3c4bec1b6cc85e53239
David Dada <daveproxy80@gmail.com>
2025-11-25 20:06:20 +0100

    Override the Diamond fallback with a straightforward delgatecall

--- test/benchmark/MinimalDiamond.sol
+++ test/benchmark/MinimalDiamond.sol
@@ -3,6 +3,23 @@ pragma solidity >=0.8.30;
 
 import {Diamond} from "@compose/Diamond.sol";
 
-// Adapted from: https://github.com/mudgen/diamond-1-hardhat/blob/main/contracts/Diamond.sol
-
-contract MinimalDiamond is Diamond {}
+contract MinimalDiamond is Diamond {
+    fallback() external payable override {
+        address facet = getDiamondStorage().facetAndPosition[msg.sig].facet;
+        if (facet == address(0)) {
+            revert FunctionDoesNotExist(msg.sig);
+        }
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+}
