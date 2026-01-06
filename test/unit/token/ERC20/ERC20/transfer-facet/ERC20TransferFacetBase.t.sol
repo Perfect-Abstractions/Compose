16d25efab3184bd63432002eba80399cb6a54d1d
lumoswiz <lumoswiz@protonmail.com>
2026-01-06 21:10:21 +0100

    test: add ERC20TransferFacet transfer unit tests

--- test/unit/token/ERC20/ERC20/transfer-facet/ERC20TransferFacetBase.t.sol
+++ test/unit/token/ERC20/ERC20/transfer-facet/ERC20TransferFacetBase.t.sol
@@ -5,15 +5,11 @@ pragma solidity >=0.8.30 <0.9.0;
  * https://compose.diamonds
  */
 
-import {stdError} from "forge-std/StdError.sol";
 import {Base_Test} from "test/Base.t.sol";
-import {ERC20StorageUtils} from "test/utils/storage/ERC20StorageUtils.sol";
 import {ERC20TransferFacet} from "src/token/ERC20/ERC20/ERC20TransferFacet.sol";
 
 /// @dev BTT spec: test/trees/ERC20.tree
 contract ERC20TransferFacet_Base_Test is Base_Test {
-    using ERC20StorageUtils for address;
-
     ERC20TransferFacet internal facet;
 
     function setUp() public virtual override {
