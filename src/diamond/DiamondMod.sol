b797504693ee299a9b41f8c73296a95ad0e23621
Nick Mudge <nick@perfectabstractions.com>
2026-02-12 22:02:06 -0500

    updated DiamondMod

--- src/diamond/DiamondMod.sol
+++ src/diamond/DiamondMod.sol
@@ -8,21 +8,15 @@ pragma solidity >=0.8.30;
 /*
 * @title Diamond Module
 * @notice Internal functions and storage for diamond proxy functionality.
-* @dev Follows EIP-2535 Diamond Standard
-* (https://eips.ethereum.org/EIPS/eip-2535)
+* @dev Follows EIP-8153 Facet-Based Diamonds
 */
 
 interface IFacet {
-    function packedSelectors() external pure returns (bytes memory);
+    function exportSelectors() external pure returns (bytes memory);
 }
 
-bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("erc8109.diamond");
+bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("erc8153.diamond");
 
-/**
- * @notice Data stored for each function selector
- * @dev Facet address of function selector
- *      Position of selector in the 'bytes4[] selectors' array
- */
 struct FacetNode {
     address facet;
     bytes4 prevFacetNodeId;
@@ -30,10 +24,10 @@ struct FacetNode {
 }
 
 struct FacetList {
+    bytes4 headFacetNodeId;
+    bytes4 tailFacetNodeId;
     uint32 facetCount;
     uint32 selectorCount;
-    bytes4 firstFacetNodeId;
-    bytes4 lastFacetNodeId;
 }
 
 /**
@@ -44,30 +38,81 @@ struct DiamondStorage {
     FacetList facetList;
 }
 
-function getStorage() pure returns (DiamondStorage storage s) {
+function getDiamondStorage() pure returns (DiamondStorage storage s) {
     bytes32 position = DIAMOND_STORAGE_POSITION;
     assembly {
         s.slot := position
     }
 }
 
-error FunctionSelectorsCallFailed(address _facet);
-error NoSelectorsForFacet(address _facet);
-error NoBytecodeAtAddress(address _contractAddress);
-error IncorrectSelectorsEncoding(address _facet);
+/**
+ * @notice Emitted when a facet is added to a diamond.
+ * @dev The function selectors this facet handles can be retrieved by calling
+ *      `IFacet(_facet).exportSelectors()`
+ *
+ * @param _facet The address of the facet that handles function calls to the diamond.
+ */
+event FacetAdded(address indexed _facet);
 
 /**
- * @notice Emitted when a function is added to a diamond.
+ * @notice Emitted when an existing facet is replaced with a new facet.
+ * @dev
+ * - Selectors that are present in the new facet but not in the old facet are added to the diamond.
+ * - Selectors that are present in both the new and old facet are updated to use the new facet.
+ * - Selectors that are not present in the new facet but are present in the old facet are removed from
+ *   the diamond.
  *
- * @param _selector The function selector being added.
- * @param _facet    The facet address that will handle calls to `_selector`.
+ * The function selectors handled by these facets can be retrieved by calling:
+ * - `IFacet(_oldFacet).exportSelectors()`
+ * - `IFacet(_newFacet).exportSelectors()`
+ *
+ * @param _oldFacet The address of the facet that previously handled function calls to the diamond.
+ * @param _newFacet The address of the facet that now handles function calls to the diamond.
  */
-event DiamondFunctionAdded(bytes4 indexed _selector, address indexed _facet);
+event FacetReplaced(address indexed _oldFacet, address indexed _newFacet);
 
+/**
+ * @notice Emitted when a facet is removed from a diamond.
+ * @dev The function selectors this facet handles can be retrieved by calling
+ *      `IFacet(_facet).exportSelectors()`
+ *
+ * @param _facet The address of the facet that previouly handled function calls to the diamond.
+ */
+event FacetRemoved(address indexed _facet);
+
+/**
+ * @notice Emitted when a diamond's constructor function or function from a
+ *         facet makes a `delegatecall`.
+ *
+ * @param _delegate         The contract that was delegatecalled.
+ * @param _delegateCalldata The function call, including function selector and
+ *                          any arguments.
+ */
+event DiamondDelegateCall(address indexed _delegate, bytes _delegateCalldata);
+
+/**
+ * @notice Emitted to record information about a diamond.
+ * @dev    This event records any arbitrary metadata.
+ *         The format of `_tag` and `_data` are not specified by the
+ *         standard.
+ *
+ * @param _tag   Arbitrary metadata, such as a release version.
+ * @param _data  Arbitrary metadata.
+ */
+event DiamondMetadata(bytes32 indexed _tag, bytes _data);
+
+/**
+ * @notice The upgradeDiamond function below detects and reverts
+ *         with the following errors.
+ */
+error NoSelectorsForFacet(address _facet);
+error NoBytecodeAtAddress(address _contractAddress);
 error CannotAddFunctionToDiamondThatAlreadyExists(bytes4 _selector);
+error FunctionSelectorsCallFailed(address _facet);
+error IncorrectSelectorsEncoding(address _facet);
 
-function packedSelectors(address _facet) view returns (bytes memory selectors) {
-    (bool success, bytes memory data) = _facet.staticcall(abi.encodeWithSelector(IFacet.packedSelectors.selector));
+function importSelectors(address _facet) view returns (bytes memory selectors) {
+    (bool success, bytes memory data) = _facet.staticcall(abi.encodeWithSelector(IFacet.exportSelectors.selector));
     if (success == false) {
         revert FunctionSelectorsCallFailed(_facet);
     }
@@ -126,19 +171,20 @@ function at(bytes memory selectors, uint256 index) pure returns (bytes4 selector
          * 1. Calculate Pointer
          * add(selectors, 32) - skips the length field of the bytes array
          * shl(2, index) is the same as index * 4 but cheaper
+         * This line executes: ptr = selectorsLength + (4 * index)
          */
         let ptr := add(add(selectors, 32), shl(2, index))
         /**
          * 2. Load & Return
          * We load 32 bytes, but Solidity truncates to 4 bytes automatically
-         * upon return assignment, so masking is unnecessary.
+         * upon return of this function, so masking is unnecessary.
          */
         selector := mload(ptr)
     }
 }
 
 function addFacets(address[] memory _facets) {
-    DiamondStorage storage s = getStorage();
+    DiamondStorage storage s = getDiamondStorage();
     uint256 facetLength = _facets.length;
     if (facetLength == 0) {
         return;
@@ -159,27 +205,36 @@ function addFacets(address[] memory _facets) {
      * For the first facet, we link the first selector to the previous facet or if this is the first facet in
      * the diamond then we assign the first selector to facetList.firstFacetNodeId.
      *
-     * Then we emit the DiamondFunctionAdded event for the first selector. But don't actually add the first
-     * selector to the diamond at this point because we don't have the nextFacetNodeId value for the facet yet.
-     * We emit the DiamondFunctionAdded event at this point so the event order is consistent with the order of
-     * selectors returned by the facet.
-     *
      * All the selectors (except the first one) in the first facet are then added to the diamond.
      *
      * In the first iteration of the main facet loop the the selectors for the next facet are retrieved.
      * This makes available the nextFacetNodeId value that is needed to store the first selector of the
      * first facet. So then the first selector is stored.
      *
-     * The main facet loop continues in a similar way, emitting the DiamondFunctionAdded for the first
-     * selector of the facet, but not adding it, adding the other selectors, and in the next iteration adding
-     * the first selector after nextFacetNodeId is available.
+     * Then the selectors which were already retrieved for the next facet are stored, except the first selector.
+     * Then in the next iteration the selectors of the next facet are retrieved. This makes available the nextFacetNodeId
+     * value that is needed to store the first selector of the previous facet. The first selector is then stored. The loop
+     * continues.
      *
      * After the main facet loop ends, the first selector from the last facet is added to the diamond.
      */
 
-    bytes4 prevFacetNodeId = facetList.lastFacetNodeId;
+    bytes4 prevFacetNodeId = facetList.tailFacetNodeId;
     address facet = _facets[0];
-    bytes memory selectors = packedSelectors(facet);
+    bytes memory selectors = importSelectors(facet);
+    /*
+     * currentFacetNodeId is the head node of the current facet.
+     * We cannot write it to storage yet because we don't know the `next` pointer.
+     */
+    bytes4 currentFacetNodeId = at(selectors, 0);
+    if (facetList.facetCount == 0) {
+        facetList.headFacetNodeId = currentFacetNodeId;
+    } else {
+        /*
+         * Link the previous tail of the diamond to this new batch
+         */
+        s.facetNodes[prevFacetNodeId].nextFacetNodeId = currentFacetNodeId;
+    }
     /*
      * Shift right by 2 is the same as dividing by 4, but cheaper.
      * We do this to get the number of selectors
@@ -189,23 +244,6 @@ function addFacets(address[] memory _facets) {
         facetList.selectorCount += uint32(selectorsLength);
     }
     /*
-     * currentFacetNodeId is the head node of the current facet.
-     * We cannot write it to storage yet because we don't know the `next` pointer.
-     */
-    bytes4 currentFacetNodeId = at(selectors, 0);
-    if (facetList.facetCount == 0) {
-        facetList.firstFacetNodeId = currentFacetNodeId;
-    } else {
-        /*
-         * Link the previous tail of the diamond to this new batch
-         */
-        s.facetNodes[prevFacetNodeId].nextFacetNodeId = currentFacetNodeId;
-    }
-    /*
-     * Emit event for the first selector now to preserve order, even though storage write is deferred.
-     */
-    emit DiamondFunctionAdded(currentFacetNodeId, facet);
-    /*
      * Add all selectors, except the first, to the diamond.
      */
     for (uint256 selectorIndex = 1; selectorIndex < selectorsLength; selectorIndex++) {
@@ -214,7 +252,6 @@ function addFacets(address[] memory _facets) {
             revert CannotAddFunctionToDiamondThatAlreadyExists(selector);
         }
         s.facetNodes[selector] = FacetNode(facet, bytes4(0), bytes4(0));
-        emit DiamondFunctionAdded(selector, facet);
     }
     /*
      * Reset memory for the main loop.
@@ -227,25 +264,14 @@ function addFacets(address[] memory _facets) {
      * 1. Gets the next facet's selectors.
      * 2. Now that the nextFacetNodeId value for the previous facet is available, adds the previous
      *    facet's first selector to the diamond.
+     * 3. Emits FacetAdded event for the previous facet.
      * 3. Updates facet values: facet = nextFacet, etc.
-     * 4. Emits the DiamondFunctionAdded for facet's first selector.
-     * 5. Adds all the selectors (except the first) to the diamond.
-     * 6. Repeat loop.
-     * Note: All selectors of a facet, except the first selector, are added the diamond. After that the first
-     * selector is added. However the DiamondEvent event for the first selector is emitted before the rest of
-     * the selectors. This maintains the order of events with the order given by facets.
+     * 4. Adds all the selectors (except the first) to the diamond.
+     * 5. Repeat loop.
      */
     for (uint256 i = 1; i < facetLength; i++) {
         address nextFacet = _facets[i];
-        selectors = packedSelectors(nextFacet);
-        /*
-         * Shift right by 2 is the same as dividing by 4, but cheaper.
-         * We do this to get the number of selectors.
-         */
-        selectorsLength = selectors.length >> 2;
-        unchecked {
-            facetList.selectorCount += uint32(selectorsLength);
-        }
+        selectors = importSelectors(nextFacet);
         /*
          * Check to see if the PENDING first selector (from previous iteration) already exists in the diamond.
          */
@@ -260,6 +286,7 @@ function addFacets(address[] memory _facets) {
          * Store the previous facet's first selector.
          */
         s.facetNodes[currentFacetNodeId] = FacetNode(facet, prevFacetNodeId, nextFacetNodeId);
+        emit FacetAdded(facet);
         /*
          * Move pointers forward.
          * These assignments switch us from processing the previous facet's first selector to
@@ -270,12 +297,10 @@ function addFacets(address[] memory _facets) {
         prevFacetNodeId = currentFacetNodeId;
         currentFacetNodeId = nextFacetNodeId;
         /*
-         * Here we emit the DiamondFunctionAdded event for for the first selector of the facet.
-         * But we don't actually add the selector here.
-         * The selector gets added in the next iteration of the loop when the nextFacetNodeId
-         * value is available for it.
+         * Shift right by 2 is the same as dividing by 4, but cheaper.
+         * We do this to get the number of selectors.
          */
-        emit DiamondFunctionAdded(currentFacetNodeId, facet);
+        selectorsLength = selectors.length >> 2;
         /*
          * Add all the selectors of the facet to the diamond, except the first selector.
          */
@@ -285,10 +310,12 @@ function addFacets(address[] memory _facets) {
                 revert CannotAddFunctionToDiamondThatAlreadyExists(selector);
             }
             s.facetNodes[selector] = FacetNode(facet, bytes4(0), bytes4(0));
-            emit DiamondFunctionAdded(selector, facet);
+        }
+        unchecked {
+            facetList.selectorCount += uint32(selectorsLength);
         }
         /*
-         * Restore Free Memory Pointer to reuse memory from packedSelector() calls.
+         * Restore Free Memory Pointer to reuse memory from packedSelectors() calls.
          */
         assembly ("memory-safe") {
             mstore(0x40, freeMemPtr)
@@ -301,9 +328,10 @@ function addFacets(address[] memory _facets) {
         revert CannotAddFunctionToDiamondThatAlreadyExists(currentFacetNodeId);
     }
     s.facetNodes[currentFacetNodeId] = FacetNode(facet, prevFacetNodeId, bytes4(0));
+    emit FacetAdded(facet);
     facetList.facetCount += uint32(facetLength);
 
-    facetList.lastFacetNodeId = currentFacetNodeId;
+    facetList.tailFacetNodeId = currentFacetNodeId;
     s.facetList = facetList;
 }
 
@@ -314,7 +342,7 @@ error FunctionNotFound(bytes4 _selector);
  * function if a facet is found and return any value.
  */
 function diamondFallback() {
-    DiamondStorage storage s = getStorage();
+    DiamondStorage storage s = getDiamondStorage();
     /**
      * get facet from function selector
      */
