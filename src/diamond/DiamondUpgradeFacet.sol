521c2b7923585d02d97c1f51680a79e7f0acafd7
maxnorm <maxnorm@hotmail.com>
2026-02-20 21:00:24 -0500

    fix solidity coment style check

--- src/diamond/DiamondUpgradeFacet.sol
+++ src/diamond/DiamondUpgradeFacet.sol
@@ -228,7 +228,7 @@ contract DiamondUpgradeFacet {
             }
         }
 
-        // Validate ABI offset == 0x20 for a single dynamic return
+        /* Validate ABI offset == 0x20 for a single dynamic return */
         uint256 offset;
         assembly ("memory-safe") {
             offset := mload(add(data, 0x20))
