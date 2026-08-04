c4dd2b7533a30e8326a5ab92f6f0863304888e3a
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2026-08-04 18:33:02 +0530

    test(diamond): harden fallback forwarding coverage

--- test/unit/diamond/mod/fuzz/fallback.t.sol
+++ test/unit/diamond/mod/fuzz/fallback.t.sol
@@ -12,6 +12,19 @@ import {DIAMOND_TEST_STORAGE_POSITION, DispatchFacet} from "test/utils/mocks/dia
 /**
  * @dev BTT spec: test/trees/Diamond.tree
  */
+contract RawCalldataFacet {
+    function rawCalldata() external payable returns (bytes memory) {
+        return msg.data;
+    }
+
+    function exportSelectors() external pure returns (bytes memory) {
+        return bytes.concat(this.rawCalldata.selector);
+    }
+}
+
+/**
+ * @dev BTT spec: test/trees/Diamond.tree
+ */
 contract Fallback_DiamondMod_Fuzz_Unit_Test is DiamondMod_Base_Test {
     function test_ShouldRevertWithFunctionNotFoundForUnknownSelector() external {
         bytes4 unknownSelector = bytes4(keccak256("unknownFunction(uint256)"));
@@ -23,8 +36,23 @@ contract Fallback_DiamondMod_Fuzz_Unit_Test is DiamondMod_Base_Test {
         assertEq(data, abi.encodeWithSelector(DiamondMod.FunctionNotFound.selector, unknownSelector), "revert data");
     }
 
+    function test_ShouldForwardNoncanonicalTrailingCalldataByteForByte() external {
+        RawCalldataFacet rawCalldataFacet = new RawCalldataFacet();
+        harness.addFacets(_singleAddress(address(rawCalldataFacet)));
+        bytes memory callData = bytes.concat(RawCalldataFacet.rawCalldata.selector, hex"decafbad0102030405060708");
+
+        (bool success, bytes memory data) = address(harness).call(callData);
+
+        assertEq(success, true, "fallback call");
+        assertEq(data, abi.encode(callData), "raw returndata");
+        bytes memory observedCalldata = abi.decode(data, (bytes));
+        assertEq(observedCalldata, callData, "forwarded calldata");
+    }
+
     function testFuzz_ShouldPreserveContextAndReturnData(uint96 _value, uint256 _argument, address _caller) external {
         vm.assume(_caller != address(0));
+        _value = uint96(bound(_value, 0, 100 ether));
+        _argument = bound(_argument, 0, type(uint128).max);
         harness.addFacets(_singleAddress(address(dispatchFacet)));
         vm.deal(_caller, _value);
 
@@ -44,6 +72,7 @@ contract Fallback_DiamondMod_Fuzz_Unit_Test is DiamondMod_Base_Test {
     }
 
     function testFuzz_ShouldBubbleExactRevertData(uint256 _value) external {
+        _value = bound(_value, 0, type(uint128).max);
         harness.addFacets(_singleAddress(address(dispatchFacet)));
 
         (bool success, bytes memory data) = address(harness).call(abi.encodeCall(DispatchFacet.fail, (_value)));
